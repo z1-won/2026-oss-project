2662c2fcac41d650e88578c9795c779b6f23f6da
z1-won <wonw512@dgu.ac.kr>
2026-03-31 02:34:34 +0900

    feat: initial project structure setup

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -2,10 +2,11 @@ export default function MainPage() {
   return (
     <main>
       <section>{/* 헤더 */}</section>
+      <section>{/* 내비게이션 */}</section>
       <section>{/* 히어로 */}</section>
       <section>{/* 자주찾는 메뉴 */}</section>
       <section>{/* 제출 현황 */}</section>
-      <section>{/* 하단 배너/공지 */}</section>
+      <section>{/* 푸터 */}</section>
     </main>
   );
 }
