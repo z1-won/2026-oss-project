e043141ea4d0efa4119e1aab510f471351833f94
z1-won <wonw512@dgu.ac.kr>
2026-03-31 02:07:11 +0900

    feat: initial project structure setup

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,11 @@
+export default function MainPage() {
+  return (
+    <main>
+      <section>{/* 헤더 */}</section>
+      <section>{/* 히어로 */}</section>
+      <section>{/* 자주찾는 메뉴 */}</section>
+      <section>{/* 제출 현황 */}</section>
+      <section>{/* 하단 배너/공지 */}</section>
+    </main>
+  );
+}
---
+++ src/pages/submit/SubmitPage.tsx
@@ -0,0 +1,13 @@
+import React, { type JSX } from "react";
+
+export default function Submit(): JSX.Element {
+  return (
+    <>
+    <h1>타이틀 자리</h1>
+    <></>
+    <button>test</button>
+
+
+    </>
+  );
+}
